cb436addbf5d5501480a1293e97cb2a052fffe4f
abaid <khawaja.abaid@gmail.com>
2025-04-05 22:33:23 -0400

    implement

--- mlp.py
+++ mlp.py
@@ -1,10 +1,9 @@
 import jax
-from jax import random, numpy as jnp, tree, vmap, nn, tree_util
-import tensorflow as tf
-import tensorflow_datasets as tfds
+from jax import random, numpy as jnp, nn, tree_util, vmap
 from typing import Sequence, Union
 from types_util import ActivationType
 from dtypes import default_dtype
+from functools import partial
 
 
 @tree_util.register_pytree_node_class
@@ -94,60 +93,28 @@ def init_mlp_params(
     return MLP(layers)
 
 
-@jax.jit
-def forward_and_loss(params, x_batched, y_batched, eps=1e-15):
+@partial(jax.jit, static_argnames=('hidden_activation_fn', 'output_activation_fn', 'loss_fn'))
+def forward_and_loss(
+    params,
+    x_batched,
+    y_batched,
+    hidden_activation_fn: callable,
+    output_activation_fn: callable,
+    loss_fn: callable,
+    ):
     def for_single_instance(x, y):
         for layer in params.layers[:-1]:
             x = jnp.matmul(x, layer.w)
             if layer.b is not None:
                 x = x + layer.b
-            x = nn.relu(x)
+            x = hidden_activation_fn(x)
         output_layer = params.layers[-1]
         x = jnp.matmul(x, output_layer.w)
         if output_layer.b is not None:
             x = x + output_layer.b
-        # Apply softmax and compute loss
-        y_pred = nn.softmax(x)
-        y_pred = jnp.clip(y_pred, eps, 1. - eps)
-        loss = jnp.sum(y * jnp.log(y_pred))
+        # Apply outptut activation and compute loss
+        y_pred = output_activation_fn(x)
+        loss = loss_fn(y, y_pred)
         return loss
-    return -jnp.mean(vmap(
+    return jnp.mean(vmap(
         for_single_instance, in_axes=(0, 0))(x_batched, y_batched))
-
-
-@jax.jit
-def update_weights(params, grad, lr):
-    return tree.map(lambda p, g: p - lr * g, params, grad)
-
-
-batch_size = 512
-mnist_ds = tfds.load('mnist', split='train', shuffle_files=True,
-                     as_supervised=True)
-mnist_ds = mnist_ds.batch(batch_size=batch_size, drop_remainder=True)
-mnist_ds = mnist_ds.map(
-    lambda x, y: (tf.cast(tf.reshape(x, (batch_size, -1)), tf.float32) / 255.0,
-                  tf.one_hot(y, depth=10)))
-
-
-params = init_mlp_params(random.key(98765), (784, 256, 128, 64, 10))
-grad_fn = jax.value_and_grad(forward_and_loss)
-n_epochs = 100
-n_batches = 0
-lr = 0.01
-per_epoch_losses = []
-for epoch in range(1, n_epochs + 1):
-    for batch, (x, y) in enumerate(mnist_ds, start=1):
-        x = x.numpy()
-        y = y.numpy()
-        if epoch == 1:
-            n_batches += 1
-            batch_str = f"{batch}/?  "
-        else:
-            batch_str = f"{batch:3}/{n_batches:3}"
-        loss, grad = grad_fn(params, x, y)
-        params = update_weights(params, grad, lr)
-        print(f"\rEpoch: {epoch:3}/{n_epochs:3} | Batch {batch_str} | "
-              f"Loss: {loss:8.5f}", end="")
-        per_epoch_losses.append(loss)
-    print(f" | Average Loss: {jnp.mean(jnp.array(per_epoch_losses)):8.5f}", end="")
-    print()
